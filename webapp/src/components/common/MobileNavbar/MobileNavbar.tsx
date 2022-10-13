9ad42e19d7984d1090af4e75148cb8d1db734c00
Bence Papp <bencepapp1101@gmail.com>
2022-10-13 12:17:21 +0200

    🚀 mobilenav icons added

--- webapp/src/components/common/MobileNavbar/MobileNavbar.tsx
+++ webapp/src/components/common/MobileNavbar/MobileNavbar.tsx
@@ -1,8 +1,49 @@
 import React from "react";
 import s from "./MobileNavbar.module.scss";
+import { Link } from "react-router-dom";
+import { HiOutlineHome, HiOutlineUsers, HiOutlineCalendar, HiOutlineIdentification, HiOutlineViewBoards } from "react-icons/hi";
 
 const MobileNavbar = () => {
-  return <div className={s.root}>MobileNavbar</div>;
+  return (
+    <nav className={s.root}>
+      <ul>
+        <Link to="/">
+          <li className={s.active}>
+            <HiOutlineHome className={s.icon} />
+            <span>item</span>
+          </li>
+        </Link>
+
+        <Link to="/">
+          <li>
+            <HiOutlineUsers className={s.icon} />
+            <span>item</span>
+          </li>
+        </Link>
+
+        <Link to="/">
+          <li>
+            <HiOutlineCalendar className={s.icon} />
+            <span>item</span>
+          </li>
+        </Link>
+
+        <Link to="/">
+          <li>
+            <HiOutlineIdentification className={s.icon} />
+            <span>item</span>
+          </li>
+        </Link>
+
+        <Link to="/">
+          <li>
+            <HiOutlineViewBoards className={s.icon} />
+            <span>item</span>
+          </li>
+        </Link>
+      </ul>
+    </nav>
+  );
 };
 
 export default MobileNavbar;
