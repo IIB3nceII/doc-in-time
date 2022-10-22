3c86f11db3f0287faff3b3793725d06ceda070cd
Bence Papp <bencepapp1101@gmail.com>
2022-10-22 14:43:09 +0200

    🚀 month picker

--- webapp/src/components/calendar/DatePicker/DatePicker.tsx
+++ webapp/src/components/calendar/DatePicker/DatePicker.tsx
@@ -3,12 +3,13 @@ import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { HiOutlineCheck, HiOutlineChevronDown } from "react-icons/hi";
 import s from "./DatePicker.module.scss";
-
-const MONTHS = ["january", "february", "mach", "april", "june", "july", "august", "september", "october", "november", "december"];
+import { MONTHS } from "src/utils/constants";
 
 const DatePicker = () => {
   const [years, setYears] = useState<number[]>([]);
   const [selectedYear, setSelectedYear] = useState<number>(years[0]);
+  const [months, setMonths] = useState<string[]>(MONTHS);
+  const [selectedMonth, setSelectedMonth] = useState<string>(MONTHS[new Date().getMonth() - 1]);
 
   useEffect(() => {
     generateYears();
@@ -29,7 +30,7 @@ const DatePicker = () => {
 
   return (
     <div className={s.container}>
-      <div className={s.yearContainer}>
+      <div className={s.listbox}>
         <Listbox value={selectedYear} onChange={setSelectedYear}>
           <div className={s.listboxContainer}>
             <Listbox.Button className={s.listboxButton}>
@@ -51,6 +52,43 @@ const DatePicker = () => {
                     {({ selected }) => (
                       <>
                         <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>{year}</span>
+                        {selected ? (
+                          <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-blue">
+                            <HiOutlineCheck className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </Listbox>
+      </div>
+
+      <div className={s.listbox}>
+        <Listbox value={selectedMonth} onChange={setSelectedMonth}>
+          <div className={s.listboxContainer}>
+            <Listbox.Button className={s.listboxButton}>
+              <span className={s.selectedYear}>{selectedMonth}</span>
+              <span className={s.dropdownIconContainer}>
+                <HiOutlineChevronDown className={s.icon} aria-hidden="true" />
+              </span>
+            </Listbox.Button>
+            <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
+              <Listbox.Options className={s.listboxOptions}>
+                {months.map((month, i) => (
+                  <Listbox.Option
+                    key={i}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? "bg-sky-300 text-white" : "text-gray-900"}`
+                    }
+                    value={month}
+                  >
+                    {({ selected }) => (
+                      <>
+                        <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>{month}</span>
                         {selected ? (
                           <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-blue">
                             <HiOutlineCheck className="h-5 w-5" aria-hidden="true" />
---
+++ webapp/src/utils/constants/index.ts
@@ -0,0 +1,1 @@
+export const MONTHS = ["January", "February", "Mach", "April", "June", "July", "August", "September", "October", "November", "December"];
