4ff5dafa0d6d40a52d37875a212f92d5aca94f82
Balint Balazs <balint.balazs@aimotive.com>
2022-10-27 01:02:47 +0200

    pagenotfound implemented

--- webapp/src/pages/PageNotFound/PageNotFound.tsx
+++ webapp/src/pages/PageNotFound/PageNotFound.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import s from "./PageNotFound.module.scss";
 import { Link } from "react-router-dom";
+import Logo from "../../assets/images/page_not_found.svg"
 
 const PageNotFound = () => {
   return (
-    <div>
-      PageNotFound <Link to="/">Return to Home Page</Link>;
+    <div className={s.root}>
+      <img src={Logo} alt="logo" />
+      <button>
+      <Link to="/">Return to the main page</Link>
+      </button>
     </div>
   );
 };
